4356c2b9386c47c117b0dd0ef19f91c5c7825a90
Moon Gyuwon <s201801402@hufs-gsuite.kr>
2021-06-20 00:16:13 +0900

    #21,22 Axios, CORS 이슈, Proxy 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios';
+
+
+// axios.get('http://localhost:5000/api/hello'
+// -> 클라이언트 포트: 3000번, 서버 포트: 5000번
+// -> 서버의 포트가 다른데 request를 보내려고 하니 CORS(Cross-Origin Resource Sharing) 정책 위반,에러 발생
+
+// 해결방법 중 하나 -- Proxy
 
 function LandingPage() {
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => console.log(response.data))
+    }, [])
+
     return (
         <div>
             LandingPage ㅎㅎ
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { auth } = require('./middleware/auth');
@@ -26,6 +25,9 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!bb'))
 
+app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요 ~ 백엔드임")
+})
 
 app.post('/api/users/register', (req, res) => {
     //회원 가입시 필요한 정보들을 client에서 가져오면
@@ -102,5 +104,5 @@ app.get('/api/users/logout', auth, (req, res) => {
         })
 })
 
-
+const port = 5000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
